89f194b6971820737b52cbdfdccf459cd05938d7
hworoshch <hworoshch@gmail.com>
2019-05-18 19:16:40 +0700

    refactored

--- interface.rb
+++ interface.rb
@@ -6,16 +6,15 @@ class Interface
   ].freeze
 
   def show_header(player_name)
-    puts "\nХодит #{player_name}:"
+    ask "Ходит #{player_name}"
   end
 
   def show_menu(menu)
     menu.each.with_index(1) { |item, index| puts "#{index}> #{item}" }
-    puts "\n"
   end
 
   def ask_name
-    puts "\nВаше имя: "
+    ask 'Ваше имя: '
     gets.chomp.to_s
   end
 
@@ -26,28 +25,35 @@ class Interface
   def show_cards(player, hidden = false)
     print "#{player.name} [банк #{player.bank}]:"
     if hidden
-      player.cards.each { print ' *' }
-      puts "\n"
+      puts player.cards.map { ' *' }.join
     else
-      player.cards.each { |card| print " #{card.suit}#{card.rank}" }
-      puts ", очки: #{player.score}\n"
+      cards = player.cards.map { |c| "#{c.suit}#{c.rank}" }
+      puts " #{cards.join(' ')}, очки: #{player.score}"
     end
   end
 
   def show_draw
-    puts "Ничья!\n"
+    say 'Ничья!'
   end
 
   def not_enough_money(player)
-    puts "#{player.name}: недостаточно денег для ставки\n"
+    say "#{player.name}: недостаточно денег для ставки"
   end
 
   def show_winner(winner)
-    puts "Победил #{winner.name}!\n"
+    say "\nПобедил #{winner.name}!"
   end
 
   def play_again?
-    puts "\nИграть еще раз? (1/0)"
+    ask 'Играть еще раз? (1/0)'
     gets.to_i == 1
+  end
+
+  def say(message)
+    puts "#{message}\n"
+  end
+
+  def ask(message)
+    puts "\n#{message}"
   end
 end
