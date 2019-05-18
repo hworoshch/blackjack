a97dcc6593406929c3b57b62524adc16574e2d6f
hworoshch <hworoshch@gmail.com>
2019-05-18 15:12:34 +0700

    corrected defaults

--- game_rules.rb
+++ game_rules.rb
@@ -2,7 +2,7 @@ module GameRules
   BJ = 21
   DEALER_MAX_POINTS = 17
   DEFAULT_BET = 10
-  DEFAULT_BANK = 30
+  DEFAULT_BANK = 100
   ACE_CORRECTION = 10
   DEFAULT_GAMER_NAME = 'Игрок'.freeze
   DEALER_NAME = 'Дилер'.freeze
