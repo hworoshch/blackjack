09a625378bd04b7192105596f1448ca97a510662
hworoshch <hworoshch@gmail.com>
2019-05-18 17:08:16 +0700

    refactored

--- main.rb
+++ main.rb
@@ -18,10 +18,9 @@ class Main
   }.freeze
 
   def run
-    create_players
     loop do
-      break interface.not_enough_money(@dealer) unless @dealer.enough_money?
-      break interface.not_enough_money(@gamer) unless @gamer.enough_money?
+      break interface.not_enough_money(dealer) unless dealer.enough_money?
+      break interface.not_enough_money(gamer) unless gamer.enough_money?
 
       reset_state
       first_deal
@@ -34,27 +33,22 @@ class Main
 
   protected
 
-  def create_players
-    @gamer = Gamer.new(interface.ask_name)
-    @dealer = Dealer.new
-  end
-
   def reset_state
-    @pack = Pack.new
-    [@gamer, @dealer].each(&:new_hand)
+    new_pack
+    [gamer, dealer].each(&:new_hand)
   end
 
   def first_deal
-    [@gamer, @dealer].each do |player|
-      player.add_cards(@pack.deal(2))
+    [gamer, dealer].each do |player|
+      player.add_cards(pack.deal(2))
       accountant.bet(player)
     end
   end
 
   def play_round
     loop do
-      interface.show_cards(@dealer, true)
-      interface.show_cards(@gamer)
+      interface.show_cards(dealer, true)
+      interface.show_cards(gamer)
       gamer_action = gamer_turn
       break if gamer_action == GAMER_ACTIONS[:open_cards]
 
@@ -64,15 +58,15 @@ class Main
   end
 
   def gamer_turn
-    interface.show_header(@gamer.name)
+    interface.show_header(gamer.name)
     interface.show_menu(Interface::PLAYER_MENU)
     case interface.players_choice
     when 1
       GAMER_ACTIONS[:skip]
     when 2
-      return unless @gamer.can_take_card?
+      return unless gamer.can_take_card?
 
-      @gamer.add_cards(@pack.deal)
+      gamer.add_cards(pack.deal)
       GAMER_ACTIONS[:take_card]
     when 3
       GAMER_ACTIONS[:open_cards]
@@ -80,25 +74,25 @@ class Main
   end
 
   def dealer_turn
-    interface.show_header(@dealer.name)
-    return unless @dealer.can_take_card?
+    interface.show_header(dealer.name)
+    return unless dealer.can_take_card?
 
-    @dealer.add_cards(@pack.deal)
+    dealer.add_cards(pack.deal)
   end
 
   def show_hands
-    [@dealer, @gamer].each { |player| interface.show_cards(player) }
+    [dealer, gamer].each { |player| interface.show_cards(player) }
   end
 
   def round_ended?
-    !@gamer.can_take_card? && !@dealer.can_take_card?
+    !gamer.can_take_card? && !dealer.can_take_card?
   end
 
   def round_results
     winner = define_winner
     if winner.nil?
       interface.show_draw
-      accountant.refund([@dealer, @gamer])
+      accountant.refund([dealer, gamer])
     else
       interface.show_winner(winner)
       accountant.reward_winner(winner)
@@ -106,12 +100,12 @@ class Main
   end
 
   def define_winner
-    return if @gamer.score == @dealer.score
-    return if @gamer.excess? && @dealer.excess?
-    return @dealer if @gamer.excess?
-    return @gamer if @dealer.excess?
+    return if gamer.score == dealer.score
+    return if gamer.excess? && dealer.excess?
+    return dealer if gamer.excess?
+    return gamer if dealer.excess?
 
-    [@gamer, @dealer].max_by(&:score)
+    [gamer, dealer].max_by(&:score)
   end
 
   private
@@ -122,5 +116,21 @@ class Main
 
   def accountant
     @accountant ||= Accountant.new
+  end
+
+  def gamer
+    @gamer ||= Gamer.new(interface.ask_name)
+  end
+
+  def dealer
+    @dealer ||= Dealer.new
+  end
+
+  def pack
+    @pack ||= Pack.new
+  end
+
+  def new_pack
+    @pack = Pack.new
   end
 end
